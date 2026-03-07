9933d5b59314a2c7a660e4b3eff6e3d0cd5f767b
sharwan jung kunwar <sharwankunwar07@gmail.com>
2026-03-07 13:23:06 +0545

    added blog

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -22,7 +22,7 @@ function BlogPage() {
   const [selectedBlog, setSelectedBlog] = useState(null);
 
   return (
-    <div className="max-w-4xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto p-4 pt-20">
 
       <h1 className="text-3xl font-bold mb-6">My Blog</h1>
 
